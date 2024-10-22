b789d3dea4836904db76c996dc46c38be1af6bd8
ananas-csun <ananas.ece25@gmail.com>
2024-10-22 01:20:53 -0700

    Added template for the Stepper Motor Controller lab

---
+++ Stepper_Motor_Controller/main.c
@@ -0,0 +1,30 @@
+/*
+ * @file main.c
+ *
+ * @brief Main source code for the Stepper_Motor_Controller program.
+ *
+ * This file contains the main entry point and function definitions for the Stepper_Motor_Controller program.
+ * This lab demonstrates how to control a stepper motor using GPIO.
+ *
+ * The following components are used:
+ *	- PMOD BTN module
+ *	-	28BYJ-48 5V Stepper Motor
+ *  - ULN2003 Stepper Motor Driver
+ *  - 3.3V / 5V Breadboard Power Supply Module (External Power Source)
+ *
+ * It configures the pins used by the PMOD BTN module to generate interrupts on rising edges.
+ * The PMOD BTN module will be used to control the rotation of the stepper motor.
+ *
+ * @author
+ */
+
+#include "TM4C123GH6PM.h"
+
+int main(void)
+{
+
+	while(1)
+	{
+
+	}
+}
